58adc7b19f93c6bde2625b7b28286e0a60b5475c
kushagra-abc <kushagraraj04@gmail.com>
2026-01-29 20:03:23 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS code as newer version
+
+//alert("hello") // we r using nodejs,not browser
+
+console.log(3 + 3); // code readablity should be high
+
+console.log("Hit")
+
+
+let name = "KUSH"
+
+let age=20
+let isLogin= false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean =>t/f
+//null =>standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)// object
