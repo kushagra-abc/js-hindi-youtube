85b0e052387935398032acb998b5c89e981dbc0d
kushagra-abc <kushagraraj04@gmail.com>
2026-02-01 12:05:24 +0000

    summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -34,3 +34,18 @@ const myFunction = function(){
 }
 
 console.log(typeof heros);
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
