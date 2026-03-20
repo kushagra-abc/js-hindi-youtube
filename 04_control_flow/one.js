dfe16222ae8961cea5acb1a3ee5f776693456f73
kushagra-abc <kushagraraj04@gmail.com>
2026-03-20 08:42:41 +0000

    conditions

---
+++ 04_control_flow/one.js
@@ -0,0 +1,42 @@
+//if
+const isUserloggesIn= true
+const temp   = 41
+
+// if(temp===41){
+//     console.log("less 50");
+
+// }
+// console.log("temp more 50")
+//<,>,<= , >= , ==, !=, ===, !==
+
+// const score=200
+
+// if(score>100){
+//     const power='fly'
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power:${power}`);
+
+
+const balance =1000
+// if(balance> 500)console.log("test"), console.log("test2"); mulitiple log with comma not good practice
+//if(balance> 500)console.log("test");            //w/o scope if statement, only once log
+
+// if(balance<500){
+//     confirm.log("less than 500")
+// }else if(balance<750){
+//     console.log("less than 750")
+// }else if(balance<900){
+//     console.log("less 900")
+// }else{
+//     console.log("less 1200")
+// }
+
+const userr=true;
+const card=true;
+const site=google;
+
+if (userr && card) {
+    console.log("allow")
+}
