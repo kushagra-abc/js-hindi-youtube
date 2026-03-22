6ec8e96ef1693456d176b3cccfd2a88ccc73945a
kushagra-abc <kushagraraj04@gmail.com>
2026-03-22 08:42:00 +0000

    more iterations

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myObject  ={
+    js:'javascript',
+    cpp:'CPP',
+    rb:'ruby',
+    swift:'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut for ${myObject[key]}`);
+    
+    
+}
+
+const programming= ["js","rb","py", "java","cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]); 
+}
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA', "United states of america")
+// map.set('Fr',"France")
+
+// map.set('USA', "United states of america")
+// map.set('Fr',"France")
+// for (const key in map) {
+//     console.log(key);   
+// }
+
